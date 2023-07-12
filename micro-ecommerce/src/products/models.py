512fa73da9c33928692c222d25222ba0a4e636fa
prathishbv <bvprathish@gmail.com>
2023-07-12 12:34:23 +0530

    product download link is ready

--- micro-ecommerce/src/products/models.py
+++ micro-ecommerce/src/products/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 from django.utils import timezone
+from django.urls import reverse
+import pathlib
+
 # Create your models here.
+
+PROTECTED_MEDIA_ROOT = settings.PROTECTED_MEDIA_ROOT
+protected_storage = FileSystemStorage(location=str(PROTECTED_MEDIA_ROOT))
 
 class Product(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
@@ -24,4 +31,29 @@ class Product(models.Model):
         super().save(*args, **kwargs)
     
     def get_absolute_url(self):
-        return f"/products/{self.handle}"
+        return reverse("products:detail", kwargs={"handle": self.handle})
+    
+    
+def handle_product_attachment_upload(instance, filename):
+    return f"products/{instance.product.handle}/attachment/{filename}"
+
+class ProductAttachment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    file = models.FileField(upload_to=handle_product_attachment_upload, storage=protected_storage)
+    name = models.CharField(max_length=120, blank=True, null=True)
+    is_free = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.name:
+            self.name = pathlib.Path(self.file.name).name
+        super().save(*args, **kwargs)
+    
+    @property
+    def display_name(self):
+        return self.name or pathlib.Path(self.file.name).name
+    
+    def get_download_url(self):
+        return reverse("products:download", kwargs={"handle": self.product.handle, "pk": self.pk})
--- micro-ecommerce/src/products/views.py
+++ micro-ecommerce/src/products/views.py
@@ -1,8 +1,11 @@
+import mimetypes
+
+from django.http import FileResponse, HttpResponseBadRequest
 from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 from .forms import ProductForm, ProductUpdateForm
-from .models import Product
+from .models import Product, ProductAttachment
 
 
 def product_create_view(request):
@@ -22,14 +25,13 @@ def product_list_view(request):
     object_list = Product.objects.all()
     return render(request, "products/list.html", {"object_list": object_list})
 
-
-def product_detail_view(request, handle=None):
+def product_manage_detail_view(request, handle=None):
     obj = get_object_or_404(Product, handle=handle)
-    is_owner = False
+    is_manager = False
     if request.user.is_authenticated:
-        is_owner = obj.user == request.user
+        is_manager = obj.user == request.user
     context = {"object": obj}
-    if is_owner:
+    if is_manager:
         form = ProductUpdateForm(request.POST or None, request.FILES or None, instance=obj)
         if form.is_valid():
             obj = form.save(commit=False)
@@ -37,3 +39,30 @@ def product_detail_view(request, handle=None):
             # return redirect('/products/create/')
         context['form'] = form
     return render(request, 'products/detail.html', context)
+
+
+
+def product_detail_view(request, handle=None):
+    obj = get_object_or_404(Product, handle=handle)
+    attachment = ProductAttachment.objects.filter(product=obj)
+    # attachment = obj.productattachment_set.all()
+    is_owner = False
+    if request.user.is_authenticated:
+        is_owner = obj.user == request.user
+    context = {"object": obj, "is_owner": is_owner, "attachments": attachment}
+    return render(request, 'products/detail.html', context)
+
+def product_attachment_download_view(request, handle=None, pk=None):
+    attachment = get_object_or_404(ProductAttachment, product__handle=handle, pk=pk)
+    can_download = attachment.is_free or False
+    if request.user.is_authenticated:
+        can_download = True # check ownership
+    if can_download is False:
+        return HttpResponseBadRequest()
+    file = attachment.file.open(mode='rb') # cdn -> S3 object storage
+    filename = attachment.file.name
+    content_type, _ = mimetypes.guess_type(filename)
+    response =  FileResponse(file)
+    response['Content-Type'] = content_type or 'application/octet-stream'
+    response['Content-Disposition'] = f'attachment;filename={filename}'
+    return response
