c3d7b7baf762db308816563b9a94117d5523431e
prathishbv <bvprathish@gmail.com>
2023-07-14 10:40:34 +0530

    aws configuration done

--- micro-ecommerce/src/cfehome/settings.py
+++ micro-ecommerce/src/cfehome/settings.py
@@ -132,3 +132,5 @@ PROTECTED_MEDIA_ROOT = BASE_DIR.parent / "local-cdn" / "protected"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from cfehome.storages.config import *
